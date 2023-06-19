951e162bc4e05e8c732750e481135ea97750e51f
Romain Vicidomini <romain.vicidomini@gmail.com>
2023-06-19 09:42:10 +0200

    modification des icones about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,87 @@ import './about.scss';
 import React, { useRef, useState } from 'react';
 import { scroller } from 'react-scroll';
 import photo from '../../assets/images/photoprofil.jpg';
-import icones from '../../data/icones';
+
+import icone1 from '../../assets/icones/react.svg';
+import icone2 from '../../assets/icones/typescript.svg';
+import icone3 from '../../assets/icones/nextjs.svg';
+import icone4 from '../../assets/icones/javascript.svg';
+import icone5 from '../../assets/icones/html.svg';
+import icone6 from '../../assets/icones/css.svg';
+import icone7 from '../../assets/icones/nodejs.svg';
+import icone8 from '../../assets/icones/bootstrap.svg';
+import icone9 from '../../assets/icones/tailwind-css.svg';
+import icone10 from '../../assets/icones/sass.svg';
+import icone11 from '../../assets/icones/postgresql.svg';
+
+const icones = [
+  {
+    id: 1,
+    src: icone1,
+    alt: 'Image react',
+    label: 'React',
+  },
+  {
+    id: 11,
+    src: icone2,
+    alt: 'Image typescript',
+    label: 'Typescript',
+  },
+  {
+    id: 2,
+    src: icone3,
+    alt: 'Image nextjs',
+    label: 'Next.js',
+  },
+  {
+    id: 3,
+    src: icone4,
+    alt: 'Image javascript',
+    label: 'Javascript',
+  },
+  {
+    id: 4,
+    src: icone5,
+    alt: 'Image html',
+    label: 'html',
+  },
+  {
+    id: 5,
+    src: icone6,
+    alt: 'Image css',
+    label: 'css',
+  },
+  {
+    id: 6,
+    src: icone7,
+    alt: 'Image nodejs',
+    label: 'Node.js',
+  },
+  {
+    id: 7,
+    src: icone8,
+    alt: 'Image bootstrap',
+    label: 'Bootstrap',
+  },
+  {
+    id: 8,
+    src: icone9,
+    alt: 'Image tailwind',
+    label: 'Tailwind',
+  },
+  {
+    id: 9,
+    src: icone10,
+    alt: 'Image sass',
+    label: 'Sass',
+  },
+  {
+    id: 10,
+    src: icone11,
+    alt: 'Image postgresql',
+    label: 'Postgresql',
+  },
+];
 
 function About() {
   const aboutRef = useRef(null);
