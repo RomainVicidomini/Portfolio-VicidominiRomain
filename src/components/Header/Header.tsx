716f592c268f410ecbf9418c13cd120c6e75b991
Romain Vicidomini <romain.vicidomini@gmail.com>
2023-06-20 07:56:18 +0200

    ajout button download CV

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,15 +20,26 @@ function Header() {
     window.addEventListener('scroll', posHeader);
   }, []);
 
+  const handleDownload = () => {
+    fetch('CV_RomainVicidomini.pdf').then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        const alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = 'CV_RomainVicidomini.pdf';
+        alink.click();
+      });
+    });
+  };
   return (
     <header ref={headerRef} className="header">
       <div className="header__left">
         <button type="button">
           <img className="header__menu" src={logo} alt="Logo" />
         </button>
-        <a href="#" className="header__logo">
-          Romain.V
-        </a>
+        <h1 className="header__title">Romain.V</h1>
       </div>
       <nav className="header__link">
         <a href="#home" className="header__active">
@@ -38,7 +49,11 @@ function Header() {
         <a href="#education">Expériences</a>
         <a href="#projets">Projets</a>
         <a href="#contact">Contact</a>
-        <button type="button" className="header__resume">
+        <button
+          type="button"
+          onClick={handleDownload}
+          className="header__resume"
+        >
           Resume
         </button>
       </nav>
